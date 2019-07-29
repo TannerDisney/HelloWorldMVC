3f2694e0ef52960da325a7cf2cd0925dbd8ad4c2
Tanner Disney <disneytanner@gmail.com>
2019-07-29 11:33:54 -0700

    Added Javascript and CSS for Customization

---
+++ AlmostEmptyWebsite/wwwroot/js/script.js
@@ -0,0 +1,22 @@
+﻿let counter = 0;
+function clicker()
+{
+    counter += 1;
+    let score = document.getElementById("score");
+    score.innerHTML = "Score : " + counter;
+    switch (counter)
+    {
+        case 50:
+            alert("Congrats on getting to 50 Clicks!");
+            break;
+        case 100:
+            alert("Congrats on getting to 100 Clicks!");
+            break;
+        case 250:
+            alert("Congrats on getting to 250 Clicks!");
+            break;
+        case 500:
+            alert("Congrats on getting to 500 Clicks!");
+            break;
+    }
+}
