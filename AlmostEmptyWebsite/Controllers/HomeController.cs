dd24469c31ad5f9d12c7b590867c97c4a94d9770
Tanner Disney <disneytanner@gmail.com>
2019-07-29 11:33:36 -0700

    Added MainController with 3 View and a Custom layout

---
+++ AlmostEmptyWebsite/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlmostEmptyWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Github()
+        {
+            ViewData["Github"] = "https://github.com/TannerDisney";
+            return View();
+        }
+
+        public IActionResult CookieClicker()
+        {
+            return View();
+        }
+
+    }
+}
